bec6705e3b5fe077b89220f8979e3e5d10d10c83
Max Mitschke <maxmitschke@fastmail.com>
2019-11-18 23:25:05 -0500

    Adding changes to game after gamejam
    These were changes that were made to the gamejam game after the
    old repo was archived. This is to create a new base for the game
    to be worked off of.

---
+++ Assets/Source/chasePlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class chasePlayer : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Transform goal;
+    public SphereEnemy obj;
+    public float speed;
+
+    void Start()
+    {
+        goal = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+        obj = gameObject.transform.GetChild(2).GetComponent<SphereEnemy>();
+    }
+
+    void Update()
+    {
+        if (obj.IsAttacking)
+        {
+            agent.speed = 0;
+        }
+        else
+        {
+            agent.speed = 3;
+        }
+        agent.destination = goal.position;
+    }
+}
