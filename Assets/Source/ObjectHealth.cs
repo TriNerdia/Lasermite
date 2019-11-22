36da5b17421a889a079715d7480d810b3cb10e8e
Arlodu <aronlondond@gmail.com>
2019-11-22 15:27:56 -0500

    added health orb ambience and pickup sound

--- Assets/Source/ObjectHealth.cs
+++ Assets/Source/ObjectHealth.cs
@@ -11,6 +11,7 @@ public class ObjectHealth : MonoBehaviour
     public bool IsDead { get { return Health <= 0; } }
     public TMPro.TMP_Text HPText;
     Animation enemyFlicker;
+    public AudioSource healSound;
 
     public void Start()
     {
@@ -70,6 +71,7 @@ public class ObjectHealth : MonoBehaviour
             if (collision.gameObject.tag == "power up")
             // detects collision with an object named power up, duplicate script to add different kinds
             {
+                healSound.Play(0);
                 //Remove the pickup from game
                 Destroy(collision.gameObject);
                 //increase health or add other affect to player
